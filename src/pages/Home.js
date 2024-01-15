219492f6bdcd8b8517ebf84713b82a38aa69dd52
mahrarhussain07 <mahrarhussain9@gmail.com>
2024-01-15 21:51:19 +0500

    created search exercise comp

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Box } from '@mui/material';
 
-// import Exercises from '../components/Exercises';
-// import SearchExercises from '../components/SearchExercises';
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
@@ -12,8 +12,8 @@ const Home = () => {
   return (
     <Box>
       <HeroBanner />
-      {/* <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} /> */}
-      {/* <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} /> */}
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
     </Box>
   );
 };
